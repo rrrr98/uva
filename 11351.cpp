b2422d7996dfbf704f2d522f63313c20c39059d3
Kevin Halim <mugen.nikki22@gmail.com>
2017-12-11 23:23:25 +0700

    Create 11351.cpp

---
+++ 11351.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int lastStanding(int n, int k)
+{
+	if (n == 1)
+		return 1;
+	else
+		return (lastStanding(n - 1, k) + k - 1) % n + 1;
+}
+int main() {
+	int nc;
+	int n, k;
+	cin >> nc;
+	for (int i = 1; i < nc; i++) {
+		cin >> n >> k;
+		cout << "Case " << i << ": ";
+		cout << lastStanding(n, k) << endl;
+	}
+	return 0;
+}
